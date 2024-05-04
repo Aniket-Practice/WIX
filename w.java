eb05561f0bbc20907af6038d14e9f7ce6e36542c
Aniket Practice <waghmareaniket150@gmail.com>
2024-05-04 12:13:20 +0530

    Cd aded

--- w.java
+++ w.java
@@ -8,6 +8,7 @@ public class w {
     public static void main(String[] args) {
         System.out.println("I am wix bot");
         System.out.println("How may I help you!");
+        System.out.println("Good morning");
 
     }
 }
